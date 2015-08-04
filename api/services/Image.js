c06298accafe8180a87281150fa25b2a9f3f93c9
Tauseef Jamadar <tauseef.jamadar@tjamad0320554.build.internal>
2015-08-03 20:18:33 -0700

    Adding Profile image to user and update image upload signature generation

--- api/services/Image.js
+++ api/services/Image.js
@@ -1,18 +1,20 @@
-(function(){
-	'use strict';
+(function() {
+    'use strict';
 
-	var cloudinary = require('cloudinary'),
-		cloudinaryUrl = require('cloudinary-url'),
-		uuid = require('node-uuid');
+    var cloudinary = require('cloudinary'),
+        cloudinaryUrl = require('cloudinary-url'),
+        uuid = require('node-uuid');
 
-	cloudinary.config(sails.config.cloudinary);
+    cloudinary.config(sails.config.cloudinary);
 
-	module.exports.getListingSignature = function(listingId, userId){
-		var urlGenerator = new cloudinaryUrl(sails.config.cloudinary.api_key, sails.config.cloudinary.api_secret, 'selbi'),
-		cloudinaryData = urlGenerator.sign({
-			public_id: userId + "/" + listingId + "/listing_" + uuid.v1()
-		});
-		return cloudinaryData;
-	};
+    module.exports.getListingSignature = function(referenceId, userId) {
+    	var urlGenerator = new cloudinaryUrl(sails.config.cloudinary.api_key, sails.config.cloudinary.api_secret, 'selbi');
+
+        var initPath = (referenceId == 0) ? userId + "/" + uuid.v1() : userId + "/" + referenceId + "/" + uuid.v1();
+        var cloudinaryData = urlGenerator.sign({
+	            public_id: initPath
+	        });
+        return cloudinaryData;
+    };
 
 })();
