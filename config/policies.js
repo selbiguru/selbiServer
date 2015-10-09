86c9577ca39e2753d752907122908dc19638f1e0
jordanharris <jordanburrows@gmail.com>
2015-10-08 19:01:02 -0700

    removed twilio from policies.js

--- config/policies.js
+++ config/policies.js
@@ -97,8 +97,5 @@ module.exports.policies = {
     'destroy':  ['authenticated', 'isAdmin'],
     'add':      ['authenticated', 'isAdmin'],
     'remove':   ['authenticated', 'isAdmin']
-  },
-   TwilioController: {
-    '*': ['authenticated']
-  },
+  }
 };
