cf519051e1bb298840ced885a03a39a90147f88c
jordanharris <jordanburrows@gmail.com>
2015-11-24 02:07:21 -0800

    Created friend'sListing route API and updated to async parallel for faster performance

--- api/controllers/ListingController.js
+++ api/controllers/ListingController.js
@@ -19,7 +19,7 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
     	});
     },
     getUserListings: function(req, res){
-    	sails.models['listing'].find({ where: { userId: req.params['userId'], sort: 'createdAt DESC' } }).exec(function(err, results){
+    	sails.models['listing'].find({ where: { userId: req.params['userId'], skip: 10 } }).exec(function(err, results){
     		if(err) 
     			return res.json(500, err);
     		return res.json(results);
@@ -36,13 +36,62 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
             });
         });
     },
-    getFriendsListings: function(req, res){
-        /*1.) have UserId need to get all friends ids
-        2.) Find friends that have at least one listing not sold
-            a.) sort by newest
-        3.) return*/
+    getFriendsListings: function(req, res){        
+        sails.services['invitationservice'].getApprovedInvitesByIdService( req.params['userId'], function(err, invitationResult) {
+            //friendsApproved = array of invitation objects
+            var friendsApproved = invitationResult;
+            var friendListings = [];
+            if(err)
+                return res.json(500, err);
+            async.eachLimit(friendsApproved, 100, function(inv, cbEach){
+                var friendId = inv.userFrom !== req.params['userId'] ? inv.userFrom : inv.userTo;
+                async.parallel([
+                    function(cb){
+                        sails.services['userservice'].getUserDataService( friendId , function(err, userResult){
+                            if(err)
+                                return cb(err);
+                            userResult.ext = 'user';
+                            cb(err, userResult);
+                        });
+                    },
+                    function(cb){
+                        var newResults;
+                        sails.models['listing'].find({ where: { userId: friendId, isSold: false} }).exec(function(err, listingResult){
+                            if(err) {
+                                return cb(err);
+                            };
+                            if(listingResult.length > 0 ) {
+                                listingResult.ext = 'listing';
+                               newResults =  _.sortByOrder(listingResult, ['createdAt'], ['desc'])[0];
+                            };
+                            cb(err, newResults);
+                        });
+                    }
+                ], function(err, results){
+                    if(err)
+                        return cbEach(err);
+                    if(results[0] && results[1]) {
+                        if(results[0].ext === 'listing') {
+                            results[0].friend = results[1];
+                            results[0].invitation = [inv];
+                            friendListings.push(results[0]);
+                        } else {
+                            results[1].friend = results[0];
+                            results[1].invitation = [inv];
+                            friendListings.push(results[1]);
+                        }
+                    }
+                    return cbEach();
+                });
+            }, function(err, results) {
+                if(err)
+                    return res.json(500, err);
+                return res.json(friendListings);
+            });
+        });
+    },
 
-        sails.services['friendservice'].getFriendsByUserService( req.params['userId'], function(err, friendsResult) {
+        /*sails.services['friendservice'].getFriendsByUserService( req.params['userId'], function(err, friendsResult) {
             //friendsResult = array of friend objects
             var friendsArray = friendsResult;
             var plop = [];
@@ -50,19 +99,25 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
                 return res.json(500,err);
             async.eachLimit(friendsArray, 100, function(friend, cbEach){
                 var friendId = friend.id;
-                sails.models['listing'].find({ where: { userId: friendId}, sort: 'title DESC' }).exec(function(err, listingResult){
+                sails.models['listing'].find({ where: { userId: friendId } }).exec(function(err, listingResult){
                     if(err) {
                         return res.json(500, err);
-                    }
-                    friend.listing = listingResult
-                    plop.push(friend);
+                    };
+                    if(listingResult.length > 0 ) {
+                        friend.listing = _.sortByOrder(listingResult, ['createdAt'], ['desc'])[0];
+                        friend.listingDate = friend.listing.createdAt;
+                        plop.push(friend);
+                    };
                     cbEach();
                 });
             }, function(err) {
                 if(err)
                     return res.json(500, err);
-                return res.json(plop);
+                return res.json(_.sortByOrder(plop, ['listingDate'], ['desc']));
             });
-        });
-    },
+        });*/
+
+
+
+
 });
