e878e03c8948b83571f4939e3b1ff1112d78cfbb
jordanharris <jordanburrows@gmail.com>
2015-11-23 19:20:10 -0800

    continuing to move controllers to services for reuse.  added find all friends of user to new friendservice.js

--- api/controllers/FriendController.js
+++ api/controllers/FriendController.js
@@ -11,25 +11,10 @@ var async = require('async');
  */
 module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
 	getFriendsByUser: function(req, res) {
-		sails.services['invitationservice'].getApprovedInvitesByIdService( req.params['userId'], function(err, approvedInvites){
-            var friendsApproved = approvedInvites;
-            var friendList = [];
-            if(err)
+		sails.services['friendservice'].getFriendsByUserService( req.params['userId'], function(err, friendsResult){
+			if(err)
                 return res.json(500, err);
-			async.eachLimit(friendsApproved, 100, function(inv, cbEach){
-				var friendId = inv.userFrom !== req.params['userId'] ? inv.userFrom : inv.userTo;
-				 sails.services['userservice'].getUserDataService( friendId , function(err, userResult){
-		            if(err)
-		                return res.json(500, err);
-					userResult.invitation = [inv];
-					friendList.push(userResult);
-					cbEach();
-		        });
-			}, function(err) {
-				if(err)
-					return res.json(500, err);
-				return res.json(friendList);
-			});
-        });
+            return res.json(friendsResult);
+		});
 	}
 });
---
+++ api/services/FriendService.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+    /**
+     * Friend Service
+     *
+     * @description :: Provides friend related calls for use throughout Selbi
+     */
+    var async = require('async');
+
+
+    /**
+     *  This is a public methods to get all friends of user by userId
+     *  @param      userId is the id of the user to find friends for
+     *  @param      cb is a callback
+     */
+    module.exports.getFriendsByUserService = function(userId, cb) {
+        sails.services['invitationservice'].getApprovedInvitesByIdService( userId, function(err, approvedInvites){
+            var friendsApproved = approvedInvites;
+            var friendList = [];
+            if(err)
+                return cb(500, err);
+            async.eachLimit(friendsApproved, 100, function(inv, cbEach){
+                var friendId = inv.userFrom !== userId ? inv.userFrom : inv.userTo;
+                 sails.services['userservice'].getUserDataService( friendId , function(err, userResult){
+                    if(err)
+                        return cb(500, err);
+                    userResult.invitation = [inv];
+                    friendList.push(userResult);
+                    cbEach();
+                });
+            }, function(err) {
+                if(err)
+                    return cb(500, err);
+                return cb(err, friendList);
+            });
+        });
+    };
+
+})();
