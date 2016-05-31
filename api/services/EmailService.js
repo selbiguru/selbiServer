040465ad2927dcea8672d297baa93c09e6a5b235
jordanharris <jordanburrows@gmail.com>
2016-05-31 13:08:19 -0700

    adding format money for email prices

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -7,6 +7,7 @@
      * @help        :: See https://github.com/mailin-api/mailin-api-node-js
      */
     require('../Mailin/mailin.js');
+    var helpers = require('./Helpers.js');
     var client = new Mailin("https://api.sendinblue.com/v2.0", sails.config.sendinblue.apikey);
 
 
@@ -100,7 +101,7 @@
                     "ADDRESSONE": lineOneAddress,
                     "ADDRESSTWO": lineTwoAddress,
                     "EMAIL": buyerData.email,
-                    "PRICE": listingData.price,
+                    "PRICE": (listingData.price).helpers.formatMoney(2),
                     "TITLE": listingData.title,
                     "REFNUM": listingData.id,
                     }
