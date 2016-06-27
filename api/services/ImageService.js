19358353332c4d23ecdd276993eb7f55c1245c3b
jordanharris <jordanburrows@gmail.com>
2016-06-27 15:37:40 -0700

    updating cloudinary configs for dev and prod

--- api/services/ImageService.js
+++ api/services/ImageService.js
@@ -9,7 +9,7 @@
     cloudinary.config(sails.config.cloudinary);
 
     module.exports.getListingSignature = function(referenceId, userId) {
-    	var urlGenerator = new cloudinaryUrl(sails.config.cloudinary.api_key, sails.config.cloudinary.api_secret, 'selbi');
+    	var urlGenerator = new cloudinaryUrl(sails.config.cloudinary.api_key, sails.config.cloudinary.api_secret, sails.config.cloudinary.cloud_name);
 
         var initPath = (referenceId == 0) ? userId + "/" + uuid.v1() : userId + "/" + referenceId + "/" + uuid.v1();
         var cloudinaryData = urlGenerator.sign({
