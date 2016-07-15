8e1425d041b8a312eaa368949f2f45819670494e
jordanharris <jordanburrows@gmail.com>
2016-07-15 02:40:23 -0700

    adding usermerchant populated user route as well a route to get a users balance from stripe.  Fixed issue with product titles being too long in the descriptor for stripe

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -33,6 +33,7 @@
      */
     module.exports.getUserDataService = function(userId, cb) {
         sails.models['user'].findOne({ where: { id: userId } }).populate('userAddress').exec(function(err, results){
+            
             if(err) {
                 sails.log.error("getUserDataService");
                 return cb(500, err);
@@ -57,6 +58,21 @@
     };
 
     /**
+     *  This is a public methods to get a user by userId and populates the merchant
+     *  @param      userId is the id of the user to find
+     *  @param      cb is a callback
+     */
+    module.exports.getUserMerchantService = function(userId, cb) {
+        sails.models['user'].findOne({ where: { id: userId } }).populate('userMerchant').exec(function(err, results){
+            if(err) {
+                sails.log.error("getUserMerchantService, can't get user by id");
+                return cb(500, err);
+            }
+            return cb(err, results);
+        });
+    };
+
+    /**
      *  This is a public methods to get a user by email
      *  @param      userEmail is the email of the user to find
      *  @param      cb is a callback
@@ -70,7 +86,6 @@
             return cb(err, results);
         });
     };
-
 
     /**
      *  This is a public methods to update a user by userId
